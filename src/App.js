0e73da54e3cc4860ec9da7e31539a40070a3a22a
rsuazo <rsuazo2@gmail.com>
2023-07-15 15:45:11 -0500

    Add card selection logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
+import React, { useState } from 'react';
+import Card from './Components/Card';
 import './App.css';
+import harry from './assets/harry.webp';
+import hermione from './assets/hermione.webp';
 
-function App() {
+const App = () => {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [visited, setVisited] = useState(new Set());
+
+  const incrementScore = () => {
+    setScore(score + 1);
+    setBestScore(bestScore + 1);
+  }
+
+  const incrementBestScore = () => {
+    setBestScore(bestScore + 1);
+  }
+  
+  const valid = (e) => {
+    let current = e.target.parentElement.getAttribute('name');
+    console.log(current);
+    console.log(visited);
+    if (!visited.has(current)) {
+      setVisited(
+        visited.add(current),
+      );
+      incrementScore();
+      return true;
+    } else {
+      resetScore();
+      alert("That Card has already been chosen!");
+      return false;
+    }
+  }
+
+  const resetScore = () => {
+    setScore(0);
+    setBestScore(0);
+    visited.clear();
+  }
+
+  const characters = ["Harry Potter", "Hermione Granger"];
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Memory Game
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className="gameBody">
+        <div className="scoreboardContainer">
+          <p>Scoreboard</p>
+          <div>Score:{score}</div>
+          <div>Best Score:{bestScore}</div>
+        </div>
+        <Card handleClick={valid} name="harry" image={harry} cardText={characters[0]}/>
+        <Card handleClick={valid} name="hermione" image={hermione} cardText={characters[1]}/>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Card.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+const Card = (props) => {
+    const [cardName, setCardName] = useState('');
+
+  return (
+    <div className="card" onClick={props.handleClick} name={props.name}>
+        <img src={props.image} alt="Avatar"></img>
+        <div className="container">
+            <h4><b>{props.cardText}</b></h4> 
+        </div>
+    </div>
+  );
+}
+
+export default Card;
