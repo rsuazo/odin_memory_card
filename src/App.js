f31a9a402571b4264240c012d06b12f939383367
rsuazo <rsuazo2@gmail.com>
2023-07-15 21:07:05 -0500

    Implement useEffect hook to shuffle cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Components/Card';
 import './App.css';
 import harry from './assets/harry.webp';
@@ -6,16 +6,31 @@ import hermione from './assets/hermione.webp';
 import ron from './assets/ron.webp';
 
 const App = () => {
+  const characters = [
+    {
+      name: "Harry Potter",
+      nickName: "harry",
+      image: harry,
+    },
+    {
+      name: "Hermione Granger",
+      nickName: "hermione",
+      image: hermione,
+    },
+    {
+      name: "Ronald Weasley",
+      nickName: "ron",
+      image: ron,
+    }
+  ]
+
   const [score, setScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [visited, setVisited] = useState(new Set());
+  const [characterList, setCharacterList] = useState(characters);
 
   const incrementScore = () => {
     setScore(score + 1);
-    setBestScore(bestScore + 1);
-  }
-
-  const incrementBestScore = () => {
     setBestScore(bestScore + 1);
   }
   
@@ -42,23 +57,48 @@ const App = () => {
     visited.clear();
   }
 
-  const characters = [
-    {
-      name: "Harry Potter",
-      nickName: "harry",
-      image: harry,
-    },
-    {
-      name: "Hermione Granger",
-      nickName: "hermione",
-      image: hermione,
-    },
-    {
-      name: "Ronald Weasley",
-      nickName: "ron",
-      image: ron,
+  const generateCharacters = () => {
+    let shuffledList = shuffle(characterList);
+    console.log(shuffledList);
+    return shuffledList;
+  }
+
+  useEffect(() => {
+    let generatedCharacters = generateCharacters();
+    setCharacterList(generatedCharacters);
+
+  },[score]);
+
+  // const checkSelection = () => {
+  //   if (valid) {
+  //     // correct choice, re-render our cards
+  //   } else {
+  //     // game over
+  //   }
+  // }
+
+
+
+
+
+  const shuffle = (array) => {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex !== 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
     }
-  ]
+  
+    console.log(array);
+    return array;
+  }
 
   return (
     <div className="App">
@@ -70,12 +110,14 @@ const App = () => {
       <div className="gameBody">
         <div className="scoreboardContainer">
           <p>Scoreboard</p>
-          <div>Score:{score}</div>
-          <div>Best Score:{bestScore}</div>
+          <div className="scoreContainer">
+            <span>Score: {score}</span>
+            <span>Best Score: {bestScore}</span>
+          </div>  
         </div>
         <div className="cardWrapper">
           {
-            characters.map((item,index) => {
+            characterList.map((item,index) => {
               return <Card handleClick={valid} name={item.nickName} image={item.image} cardText={item.name}/>
             })
           }
